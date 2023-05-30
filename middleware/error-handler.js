9de9cfd78e145cd71d120a63f1beab4980ded6a5
av124773 <av124773@gmail.com>
2023-05-30 22:42:54 +0800

    feat: add api error handler & delete restaurant api

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,14 +1,3 @@
-// module.exports = {
-//   generalErrorHandler (err, req, res, next) {
-//     if (err instanceof Error) {
-//       req.flash('error_messages', `${err.name}: ${err.message}`)
-//     } else {
-//       req.flash('error_messages', `${err}`)
-//     }
-//     res.redirect('back')
-//     next(err)
-//   }
-// }
 module.exports = {
   generalErrorHandler (err, req, res, next) {
     if (err instanceof Error) {
@@ -17,6 +6,20 @@ module.exports = {
       req.flash('error_messages', `${err}`)
     }
     res.redirect('back')
+    next(err)
+  },
+  apiErrorHandler (err, req, res, next) {
+    if (err instanceof Error) {
+      res.status(err.status || 500).json({
+        status: 'error',
+        message: `${err.name}: ${err.message}`
+      })
+    } else {
+      res.status(500).json({
+        status: 'error',
+        message: `${err}`
+      })
+    }
     next(err)
   }
 }
